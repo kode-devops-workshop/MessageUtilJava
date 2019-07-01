cec06d930c75049fc8e12e28fb3ea1327ffb2fb1
William Notowidagdo <wnotowidagdo@gmail.com>
2019-07-01 12:14:11 +0700

    fix the failing build

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -11,7 +11,7 @@ public class MessageUtil {
   }
 
   public String salutationMessage() {
-    message = "Hello!" + message;
+    message = "Hi!" + message;
     System.out.println(message);
     return message;
   }
